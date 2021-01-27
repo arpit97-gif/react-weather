000d1221cd141695aeb228c7469edc524792774f
Arpit Kumar <55889691+arpit97-gif@users.noreply.github.com>
2021-01-27 15:48:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ if(evt.key==="Enter")
  : 'app' }>
       <main>
         <div className="search">
-          <input type="text" placeholder="Search...." className="search-bar"
+          <input type="text" placeholder="ENTER the location ..." className="search-bar"
           onChange={e => setQurey(e.target.value)}
           value={query}
           onKeyPress={search}
